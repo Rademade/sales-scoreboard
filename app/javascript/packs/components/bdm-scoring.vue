f9ab45e7d37cda5ff9ac869b0a2c0d343744b3ac
STsaruk <s0936771414@gmail.com>
2018-04-05 15:56:26 +0300

    add new styles for board

--- app/javascript/packs/components/bdm-scoring.vue
+++ app/javascript/packs/components/bdm-scoring.vue
@@ -1,33 +1,31 @@
 <template>
     <div id="bdm-scoring" class="main__block">
         <table class="table">
+            <tr class="table__row">
+                <td class="table__data table__data--large"></td>
+                <td style="color: #f1c40f;" class="table__data table__data--large">Activity</td>
+                <td style="color: #3498db;" class="table__data table__data--large">New contacts</td>
+                <td style="color: #c48adc;" class="table__data table__data--large">New deals</td>
+                <td style="color: #e29e57;" class="table__data table__data--large">Decision deals</td>
+                <td style="color: #00cc56;" class="table__data table__data--large">Won deals</td>
+            </tr>
             <tr v-for="(manager, index) in sortedManagers" class="table__row">
                 <td class="table__data table__data--name table__data--large table__data--bold">{{manager.name}}</td>
                 <td style="color: #f1c40f;" class="table__data table__data--large">{{manager.activities_count}}</td>
                 <td style="color: #3498db;" class="table__data table__data--large">{{manager.person_count}}</td>
-                <td style="color: #8e44ad;" class="table__data table__data--large">{{manager.created_deals}}</td>
-                <td style="color: #16a085;" class="table__data table__data--large">{{manager.decision_deals}}</td>
-                <td style="color: #27ae60;" class="table__data table__data--large table__data--bold">{{manager.won_deals}}</td>
+                <td style="color: #c48adc;" class="table__data table__data--large">{{manager.created_deals}}</td>
+                <td style="color: #e29e57;" class="table__data table__data--large">{{manager.decision_deals}}</td>
+                <td style="color: #00cc56;" class="table__data table__data--large table__data--bold">{{manager.won_deals}}</td>
             </tr>
             <tr class="table__row">
                 <td class="table__data table__data--name table__data--large table__data--bold">Total</td>
                 <td style="color: #f1c40f;" class="table__data table__data--large">{{totalActivities}}</td>
                 <td style="color: #3498db;" class="table__data table__data--large">{{totalPersons}}</td>
-                <td style="color: #8e44ad;" class="table__data table__data--large">{{totalCreatedDeals}}/{{newDealsPlan}}</td>
-                <td style="color: #16a085;" class="table__data table__data--large">{{totalDecisionDeals}}</td>
-                <td style="color: #27ae60;" class="table__data table__data--large table__data--bold">{{totalWonDeals}}/{{wonDealsPlan}}</td>
+                <td style="color: #c48adc;" class="table__data table__data--large">{{totalCreatedDeals}}/{{newDealsPlan}}</td>
+                <td style="color: #e29e57;" class="table__data table__data--large">{{totalDecisionDeals}}</td>
+                <td style="color: #00cc56;" class="table__data table__data--large table__data--bold">{{totalWonDeals}}/{{wonDealsPlan}}</td>
             </tr>
 
-            <tr class="table__row table__row--title">
-                <td class="table__data table__data--history" colspan="6">
-                    <span style="color: #f1c40f;">Activity</span>
-                    <span style="color: #3498db;">New contacts</span>
-                    <span style="color: #8e44ad;">New deals</span>
-                    <span style="color: #16a085;">Decision deals</span>
-                    <span style="color: #27ae60;">Won deals</span>
-
-                </td>
-            </tr>
         </table>
     </div>
 </template>
